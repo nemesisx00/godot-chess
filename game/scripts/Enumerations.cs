9232c5e64c196cd7205e611a03535bf96efa8751
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-02-03 17:00:25 -0500

    Create Enumerations.cs

---
+++ game/scripts/Enumerations.cs
@@ -0,0 +1,41 @@
+namespace Chess;
+
+public enum File
+{
+	A,
+	B,
+	C,
+	D,
+	E,
+	F,
+	G,
+	H,
+}
+
+public enum Pieces
+{
+	Bishop,
+	King,
+	Knight,
+	Pawn,
+	Queen,
+	Rook,
+}
+
+public enum Rank
+{
+	One,
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+}
+
+public enum Teams
+{
+	Black,
+	White,
+}
