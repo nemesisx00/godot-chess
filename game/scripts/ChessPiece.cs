1d93c6bc84a02420411b3790d134b23d16311c23
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-02-08 15:36:52 -0500

    Update ChessPiece.cs
    Use a Tween node to smoothly move the piece to a destination.

--- game/scripts/ChessPiece.cs
+++ game/scripts/ChessPiece.cs
@@ -4,27 +4,87 @@ namespace Chess;
 
 public partial class ChessPiece : CharacterBody3D
 {
+	[Signal]
+	public delegate void ClickedEventHandler(ChessPiece piece);
+	
 	[Export]
 	public Material OverrideMaterial { get; set; }
 	
+	[Export(PropertyHint.Range, ".1,1,.1")]
+	private float moveDuration = .2f;
+	
+	private static readonly NodePath GlobalPositionPath = "global_position";
+	
+	public bool DiagonalMovement { get; set; }
+	public File File { get; set; }
+	public Rank Rank { get; set; }
 	public Teams Team { get; set; }
-
+	
+	public Vector3 Destination
+	{
+		get => destination;
+		set
+		{
+			destination = value;
+			
+			if(!Vector3.Zero.IsEqualApprox(destination))
+			{
+				tween?.Kill();
+				tween = CreateTween()
+					.BindNode(this)
+					.SetEase(Tween.EaseType.InOut)
+					.SetLoops(1)
+					.SetParallel(DiagonalMovement)
+					.SetTrans(Tween.TransitionType.Linear);
+				
+				tween.Finished += handleTweenFinished;
+				
+				if(!Mathf.IsEqualApprox(GlobalPosition.X, destination.X))
+				{
+					var one = destination;
+					one.X = GlobalPosition.X;
+					var two = destination;
+					
+					tween.TweenProperty(this, GlobalPositionPath, one, moveDuration);
+					tween.TweenProperty(this, GlobalPositionPath, two, moveDuration);
+				}
+				else
+					tween.TweenProperty(this, GlobalPositionPath, destination, moveDuration);
+			}
+		}
+	}
+	
+	private Vector3 destination;
+	
+	private bool isFalling = true;
+	private Tween tween;
+	private bool mouseActive;
+	
+	public override void _InputEvent(Camera3D camera, InputEvent evt, Vector3 position, Vector3 normal, int shapeIdx)
+	{
+		if(evt is InputEventMouseButton iemb && iemb.ButtonIndex == MouseButton.Left && iemb.Pressed)
+			EmitSignal(SignalName.Clicked, this);
+	}
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		var velocity = Velocity;
-		if(!IsOnFloor())
+		
+		if(isFalling)
+		{
 			velocity.Y -= GlobalSettings.Gravity * (float)delta;
-		else
-			velocity.Y = 0;
+			
+			if(IsOnFloor())
+				isFalling = false;
+		}
 		
 		Velocity = velocity;
-		MoveAndSlide();
+		
+		if(!Vector3.Zero.IsEqualApprox(Velocity))
+			MoveAndSlide();
 	}
 	
-	public override void _Ready()
-	{
-		ReloadTextures();
-	}
+	public override void _Ready() => ReloadTextures();
 	
 	public void ReloadTextures()
 	{
@@ -33,5 +93,13 @@ public partial class ChessPiece : CharacterBody3D
 			var mesh = GetChild<MeshInstance3D>(0);
 			mesh.MaterialOverride = OverrideMaterial;
 		}
+	}
+	
+	public void StartFalling() => isFalling = true;
+	
+	private void handleTweenFinished()
+	{
+		Destination = Vector3.Zero;
+		isFalling = true;
 	}
 }
