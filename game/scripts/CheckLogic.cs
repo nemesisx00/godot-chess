e328932f3d96bbdfde1f31fd8405467ef773d8e3
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-02-16 17:36:03 -0500

    Update CheckLogic.cs
    Abstracted some of the logic out into canPieceCauseCheck.
    Implemented PredictCheck.
    Added the bare minimum doc comments.

--- game/scripts/CheckLogic.cs
+++ game/scripts/CheckLogic.cs
@@ -1,11 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Chess;
 
 public static class CheckLogic
 {
+	/**
+	<summary>
+	Detect if a king is in check.
+	</summary>
+	*/
 	public static bool IsInCheck(ChessPiece king, Chessboard board)
 	{
 		var check = false;
@@ -27,25 +31,7 @@ public static class CheckLogic
 				{
 					var colliderCell = cp.GetParentOrNull<BoardCell>();
 					if(colliderCell is not null)
-					{
-						if(cardinal)
-							check = cp.Type == Piece.Queen || cp.Type == Piece.Rook;
-						else
-						{
-							check = cp.Type == Piece.Bishop || cp.Type == Piece.Queen;
-							if(!check)
-							{
-								var fileDist = Math.Abs(kingCell.File - colliderCell.File);
-								var rankDist = kingCell.Rank - colliderCell.Rank;
-								
-								check = cp.Type == Piece.Pawn && fileDist == 1
-									&& (
-										(cp.Team == Teams.White && rankDist == 1)
-										|| (cp.Team == Teams.Black && rankDist == -1)
-									);
-							}
-						}
-					}
+						check = canPieceCauseCheck(cp, cardinal, Math.Abs(kingCell.File - colliderCell.File), kingCell.Rank - colliderCell.Rank);
 				}
 			}
 		}
@@ -57,10 +43,84 @@ public static class CheckLogic
 				if(check)
 					break;
 				
-				check = PieceMovementLogic.GetValidCells(knight, board.Cells).Contains(kingCell);
+				check = PieceMovementLogic.GetValidCells(knight, board).Contains(kingCell);
 			}
 		}
 		
+		return check;
+	}
+	
+	/**
+	<summary>
+	Predict if moving a piece will put the player's own king in check.
+	</summary>
+	*/
+	public static bool PredictCheck(Chessboard board, ChessPiece mover)
+		=> PredictCheck(board.Pieces.Where(p => p.Type == Piece.King && p.Team == mover.Team).First(), mover);
+	
+	/**
+	<summary>
+	Predict if moving a piece will put the player's own king in check.
+	</summary>
+	*/
+	public static bool PredictCheck(ChessPiece king, ChessPiece mover)
+	{
+		//Pre-emptive evaluation to see if moving the `mover` piece will put their own `king` in check
+		var check = false;
+		
+		//Sanity check
+		if(king != mover && king.Team == mover.Team)
+		{
+			var kingCell = king.GetParent<BoardCell>();
+			var moverCell = mover.GetParent<BoardCell>();
+			
+			var fileDiff = kingCell.File - moverCell.File;
+			var rankDiff = kingCell.Rank - moverCell.Rank;
+			
+			string direction = DirectionNames.ByDifference(fileDiff, rankDiff);
+			if(!String.IsNullOrEmpty(direction))
+			{
+				var ray = king.Rays.Where(r => r.Name.Equals(direction)).FirstOrDefault();
+				if(ray is not null)
+				{
+					var cardinal = DirectionNames.IsCardinal(direction);
+					
+					ray.AddException(mover);
+					ray.ForceRaycastUpdate();
+					ray.RemoveException(mover);
+					
+					var collider = ray.GetCollider();
+					if(collider is ChessPiece cp && cp.Team != king.Team)
+						check = canPieceCauseCheck(cp, cardinal, fileDiff, rankDiff);
+				}
+			}
+		}
+		
+		return check;
+	}
+	
+	/**
+	<summary>
+	Evaluate if a given piece, based on its allowed means of movement, could cause check.
+	</summary>
+	*/
+	private static bool canPieceCauseCheck(ChessPiece piece, bool cardinal, int fileDiff, int rankDiff)
+	{
+		var check = false;
+		if(cardinal)
+			check = piece.Type == Piece.Queen || piece.Type == Piece.Rook;
+		else
+		{
+			check = piece.Type == Piece.Bishop || piece.Type == Piece.Queen;
+			if(!check)
+			{
+				check = piece.Type == Piece.Pawn && fileDiff == 1
+					&& (
+						(piece.Team == Teams.White && rankDiff == 1)
+						|| (piece.Team == Teams.Black && rankDiff == -1)
+					);
+			}
+		}
 		return check;
 	}
 }
