34fa32b34b22443f38085664bf8203363229fb41
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-02-23 16:07:36 -0500

    Update MoveLogEntry.cs
    Added Castle property, to mark a move as being a castling move.
    Updated ToString to account for the unique castling notation.

--- game/scripts/autoload/MoveLogEntry.cs
+++ game/scripts/autoload/MoveLogEntry.cs
@@ -2,30 +2,41 @@ namespace Chess.Autoload;
 
 public class MoveLogEntry(BoardVector from, BoardVector to, Piece piece, Teams team)
 {
+	private const string CastleKingside = "0-0";
+	private const string CastleQueenside = "0-0-0";
+	
 	public BoardVector From { get; set; } = from;
 	public BoardVector To { get; set; } = to;
 	public Piece Piece { get; set; } = piece;
 	public Teams Team { get; set; } = team;
-	public bool Capture { get; set; } = false;
-	public bool File { get; set; } = false;
-	public bool Rank { get; set; } = false;
+	public bool Capture { get; set; }
+	public bool File { get; set; }
+	public bool Rank { get; set; }
+	public bool Castle { get; set; }
 
 	public override string ToString()
 	{
-		var moveIsCapture = string.Empty;
-		if(Capture)
-			moveIsCapture = "x";
-		
-		var disambiguation = string.Empty;
-		if(File && Rank)
-			disambiguation = From.ToString().ToLower();
-		else if(File)
-			disambiguation = ((File)From.File).ToString().ToLower();
-		else if(Rank)
-			disambiguation = From.Rank.ToString();
+		string notation;
+		if(Castle)
+			notation = From.File - To.File > 0 ? CastleQueenside : CastleKingside;
+		else
+		{
+			var moveIsCapture = string.Empty;
+			if(Capture)
+				moveIsCapture = "x";
+			
+			var disambiguation = string.Empty;
+			if(File && Rank)
+				disambiguation = From.ToString().ToLower();
+			else if(File)
+				disambiguation = ((File)From.File).ToString().ToLower();
+			else if(Rank)
+				disambiguation = From.Rank.ToString();
+			
+			notation = $"{disambiguation}{moveIsCapture}{To.ToString().ToLower()}";
+		}
 		
 		var icon = UnicodePiece.ByTeamPiece(Team, Piece);
-		
-		return $"{icon}{disambiguation}{moveIsCapture}{To.ToString().ToLower()}";
+		return $"{icon}{notation}";
 	}
 }
