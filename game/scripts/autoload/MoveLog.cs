ad87cdfc13aa44ea793b60634ae5968946890532
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-02-23 16:07:58 -0500

    Update MoveLog.cs
    Added MoveLogged signal.
    Updated AddEntry to emit the MoveLogged signal.

--- game/scripts/autoload/MoveLog.cs
+++ game/scripts/autoload/MoveLog.cs
@@ -8,6 +8,9 @@ public partial class MoveLog : Node
 {
 	public static readonly NodePath NodePath = new("/root/MoveLog");
 	
+	[Signal]
+	public delegate void MoveLoggedEventHandler();
+	
 	List<MoveLogEntry> Entries { get; set; } = [];
 	
 	public MoveLogEntry MostRecentEntry => Entries.LastOrDefault();
@@ -15,5 +18,9 @@ public partial class MoveLog : Node
 	public void AddEntry(BoardVector from, BoardVector to, Piece piece, Teams team)
 		=> AddEntry(new(from, to, piece, team));
 	
-	public void AddEntry(MoveLogEntry entry) => Entries.Add(entry);
+	public void AddEntry(MoveLogEntry entry)
+	{
+		Entries.Add(entry);
+		EmitSignal(SignalName.MoveLogged);
+	}
 }
