ee732ab2c82cf048825f57ca55a9363d0dab32c9
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-02-24 00:02:28 -0500

    Update MoveLogEntry.cs
    Alphabetized

--- game/scripts/autoload/MoveLogEntry.cs
+++ game/scripts/autoload/MoveLogEntry.cs
@@ -5,14 +5,14 @@ public class MoveLogEntry(BoardVector from, BoardVector to, Piece piece, Teams t
 	private const string CastleKingside = "0-0";
 	private const string CastleQueenside = "0-0-0";
 	
-	public BoardVector From { get; set; } = from;
-	public BoardVector To { get; set; } = to;
-	public Piece Piece { get; set; } = piece;
-	public Teams Team { get; set; } = team;
 	public bool Capture { get; set; }
-	public bool File { get; set; }
-	public bool Rank { get; set; }
 	public bool Castle { get; set; }
+	public bool File { get; set; }
+	public BoardVector From { get; set; } = from;
+	public Piece Piece { get; set; } = piece;
+	public bool Rank { get; set; }
+	public Teams Team { get; set; } = team;
+	public BoardVector To { get; set; } = to;
 
 	public override string ToString()
 	{
