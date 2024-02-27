e83c7a31437ea048b66008db545a04093a8f09f2
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-02-27 01:22:23 -0500

    Update Credits.cs
    Added the necessary text for Godot Engine's third party libraries which are required for compliance with their licenses.

--- game/scripts/nodes/Credits.cs
+++ game/scripts/nodes/Credits.cs
@@ -1,4 +1,8 @@
 using Godot;
+using Godot.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Chess.Nodes;
 
@@ -7,10 +11,66 @@ public partial class Credits : Control
 	private static class NodePaths
 	{
 		public static readonly NodePath GodotLicense = new("%GodotLicenseText");
+		public static readonly NodePath FreeTypeLicense = new("%FreeTypeLicense");
+		public static readonly NodePath ENetLicense = new("%EnetLicense");
 	}
+	
+	private static class CopyrightKeys
+	{
+		public const string Name = "name";
+		public const string Parts = "parts";
+		public const string Copyright = "copyright";
+	}
+	
+	private static class LicenseTexts
+	{
+		public const string ENet = "Copyright (c) {dates} Lee Salzman\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+		public const string FreeType = "Portions of this software are copyright @ {dates} The FreeType Project (www.freetype.org). All rights reserved.";
+	}
+	
+	private static class ModuleNames
+	{
+		public const string ENet = "ENet";
+		public const string FreeType = "The FreeType Project";
+	}
+	
+	private const string DatesReplace = "{dates}";
+	private const string RegexReplace = "$1";
+	
+	[GeneratedRegex(@"^(\d{4}-\d{4}).*$")]
+	private static partial Regex CopyrightDatesRegex();
 	
 	public override void _Ready()
 	{
 		GetNode<Label>(NodePaths.GodotLicense).Text = Engine.GetLicenseText();
+		
+		List<string> names = [
+			ModuleNames.ENet,
+			ModuleNames.FreeType,
+		];
+		
+		Engine.GetCopyrightInfo()
+			.Where(dict => names.Contains((string)dict[CopyrightKeys.Name]))
+			.ToList()
+			.ForEach(dict => {
+				var parts = (Dictionary)((Array)dict[CopyrightKeys.Parts])[0];
+				var copyright = (string)((Array)parts[CopyrightKeys.Copyright])[0];
+				var regex = CopyrightDatesRegex();
+				
+				var dates = string.Empty;
+				if(regex.IsMatch(copyright))
+					dates = regex.Replace(copyright, RegexReplace);
+				
+				switch(dict[CopyrightKeys.Name].ToString())
+				{
+					case ModuleNames.ENet:
+						GetNode<Label>(NodePaths.ENetLicense).Text = LicenseTexts.ENet.Replace(DatesReplace, dates);
+						break;
+					
+					case ModuleNames.FreeType:
+						GetNode<Label>(NodePaths.FreeTypeLicense).Text = LicenseTexts.FreeType.Replace(DatesReplace, dates);
+						break;
+				}
+			});
 	}
 }
