5db54f5ada9f956476442ddf03cdcc7a3c57bb4c
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-02-26 20:44:49 -0500

    Update MainMenu.cs
    Added Credits node to the scene.
    
    Added a button labeled Credits which displays the Credits node when clicked; Any keyboard key will hide the Credits node and show the main menu again when released.
    
    Added readonly property CreditsVisible so that other nodes can easily detect if the credits node is visible or not.

--- game/scripts/nodes/MainMenu.cs
+++ game/scripts/nodes/MainMenu.cs
@@ -6,6 +6,8 @@ public partial class MainMenu : MarginContainer
 {
 	private static class NodePaths
 	{
+		public static readonly NodePath Credits = new("%Credits");
+		public static readonly NodePath CreditsButton = new("%CreditsButton");
 		public static readonly NodePath NewGame = new("%NewGame");
 		public static readonly NodePath Quit = new("%Quit");
 	}
@@ -13,10 +15,34 @@ public partial class MainMenu : MarginContainer
 	[Signal]
 	public delegate void StartNewGameEventHandler();
 	
+	public bool CreditsVisible => credits.Visible;
+	
+	private HBoxContainer mainRow;
+	private Credits credits;
+	
+	public override void _UnhandledInput(InputEvent evt)
+	{
+		if(credits.Visible && evt is InputEventKey iek && iek.IsReleased())
+		{
+			credits.Hide();
+			mainRow.Show();
+		}
+	}
+	
 	public override void _Ready()
 	{
+		credits = GetNode<Credits>(NodePaths.Credits);
+		mainRow = GetChild<HBoxContainer>(0);
+		
+		GetNode<Button>(NodePaths.CreditsButton).Pressed += pressedCredits;
 		GetNode<Button>(NodePaths.NewGame).Pressed += pressedNewGame;
 		GetNode<Button>(NodePaths.Quit).Pressed += pressedQuit;
+	}
+	
+	private void pressedCredits()
+	{
+		mainRow.Hide();
+		credits.Show();
 	}
 	
 	private void pressedNewGame() => EmitSignal(SignalName.StartNewGame);
