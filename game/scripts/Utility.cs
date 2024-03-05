8d0ccbed25a85adf6c583671fec4bf402ab611db
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-03-05 17:39:03 -0500

    Update Utility.cs
    Added method PrintMoveList to print a meaningful string given a list of BoardCell instances.

--- game/scripts/Utility.cs
+++ game/scripts/Utility.cs
@@ -1,9 +1,24 @@
+using System.Collections.Generic;
+using System.Text;
 using Godot;
+using Chess.Nodes;
 
 namespace Chess;
 
 public static class Utility
 {
+	public static void PrintMoveList(List<BoardCell> moves)
+	{
+		StringBuilder sb = new('[');
+		moves.ForEach(move => {
+			if(sb.Length > 1)
+				sb.Append(", ");
+			sb.Append(move.Name);
+		});
+		sb.Append(']');
+		GD.Print(sb);
+	}
+	
 	public static void SetOrigin<T>(T node, Vector3 origin)
 		where T: Node3D
 	{
