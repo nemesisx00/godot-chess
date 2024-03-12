ccf3859acbcf1d6f61df68118c1a85b44087e217
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-03-12 15:04:04 -0400

    Update CameraController.cs
    Created a new input action for the camera rotation, RotateCamera.
    
    Updated physics process to only operate when rotating is true.

--- game/scripts/Constants.cs
+++ game/scripts/Constants.cs
@@ -7,6 +7,7 @@ namespace Chess;
 public static class Actions
 {
 	public static readonly StringName DeselectPiece = "DeselectPiece";
+	public static readonly StringName RotateCamera = "RotateCamera";
 	public static readonly StringName ToggleMenu = "ToggleMenu";
 }
 
