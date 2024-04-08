621f5a399f1008f2a9e142923c862c97f540415c
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-04-08 16:47:32 -0400

    Update OptionsMenu.tscn and OptionsMenu.cs
    Added a button, Back, to the tab list for returning to the Main Menu.
    Added a new signal, RequestHide, emitted when the back button is pressed.
    Added public method ResetActiveTab to utilize showMenu to ensure the first menu is the active tab.
    Updated showMenu so the parameter, id, has a default value of 0; Calling showMenu() with no parameter will always show the very first menu, as defined by the value order within the MenuId enumeration.

--- game/scripts/nodes/OptionsMenu.cs
+++ game/scripts/nodes/OptionsMenu.cs
@@ -1,6 +1,7 @@
 using Godot;
-using System;
 using System.Linq;
+
+namespace Chess.Nodes;
 
 public partial class OptionsMenu : Container
 {
@@ -8,6 +9,7 @@ public partial class OptionsMenu : Container
 	{
 		public static readonly NodePath AudioOptions = new("%AudioOptions");
 		public static readonly NodePath AudioTab = new("%AudioTab");
+		public static readonly NodePath BackToMainMenu = new("%BackToMainMenu");
 		public static readonly NodePath ControlOptions = new("%ControlOptions");
 		public static readonly NodePath ControlTab = new("%ControlsTab");
 		public static readonly NodePath MenuContainer = new("%MenuContainer");
@@ -19,8 +21,12 @@ public partial class OptionsMenu : Container
 		Control,
 	}
 	
+	[Signal]
+	public delegate void RequestHideEventHandler();
+	
 	private Container audioNode;
 	private Button audioTab;
+	private Button back;
 	private Container controlNode;
 	private Button controlTab;
 	private Container menuContainer;
@@ -29,15 +35,19 @@ public partial class OptionsMenu : Container
 	{
 		audioNode = GetNode<Container>(NodePaths.AudioOptions);
 		audioTab = GetNode<Button>(NodePaths.AudioTab);
+		back = GetNode<Button>(NodePaths.BackToMainMenu);
 		controlNode = GetNode<Container>(NodePaths.ControlOptions);
 		controlTab = GetNode<Button>(NodePaths.ControlTab);
 		menuContainer = GetNode<Container>(NodePaths.MenuContainer);
 		
 		audioTab.Pressed += () => showMenu(MenuId.Audio);
+		back.Pressed += () => EmitSignal(SignalName.RequestHide);
 		controlTab.Pressed += () => showMenu(MenuId.Control);
 	}
 	
-	private void showMenu(MenuId id)
+	public void ResetActiveTab() => showMenu();
+	
+	private void showMenu(MenuId id = 0)
 	{
 		foreach(var child in menuContainer.GetChildren().Cast<Control>())
 		{
