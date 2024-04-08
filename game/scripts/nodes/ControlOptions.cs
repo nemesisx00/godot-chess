dadf9f410f5a41abcab3203bb9e28eec5c958b44
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-04-08 16:22:59 -0400

    Update ControlOptions.tscn and ControlOptions.cs
    Added a new PanelContainer named Popup for use in prompting the user to press a key or button when remapping an action's input event.
    Implemented functionality in ControlOptions.cs to show or hide the popup when one of its child ActionMapper nodes signals it has started or ended listening.

--- game/scripts/nodes/ControlOptions.cs
+++ game/scripts/nodes/ControlOptions.cs
@@ -4,7 +4,50 @@ namespace Chess.Nodes;
 
 public partial class ControlOptions : Container
 {
+	private static class NodePaths
+	{
+		public static readonly NodePath ControlsList = new("%ControlsList");
+		public static readonly NodePath PopupPanel = new("%Popup");
+	}
+	
+	private GridContainer controlsList;
+	private PanelContainer popup;
+	
+	//Custom signals need to be manually detached, at least for now.
+	public override void _ExitTree()
+	{
+		configureSignals(false);
+		
+		base._ExitTree();
+	}
+	
 	public override void _Ready()
 	{
+		controlsList = GetNode<GridContainer>(NodePaths.ControlsList);
+		popup = GetNode<PanelContainer>(NodePaths.PopupPanel);
+		
+		configureSignals(true);
+	}
+	
+	private void displayPopup(ActionMapper node, bool listening)
+	{
+		if(listening)
+			popup.Show();
+		else
+			popup.Hide();
+	}
+	
+	private void configureSignals(bool attach)
+	{
+		foreach(var child in controlsList.GetChildren())
+		{
+			if(child is ActionMapper am)
+			{
+				if(attach)
+					am.ListeningForInput += displayPopup;
+				else
+					am.ListeningForInput -= displayPopup;
+			}
+		}
 	}
 }
