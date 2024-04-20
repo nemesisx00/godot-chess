1a3a3642e7e9a9591c5a39a62e92fcbaffb26538
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-04-20 15:19:21 -0400

    Update GodotPaths.cs
    Added constants for the five style override types on buttons.

--- game/scripts/constants/GodotPaths.cs
+++ game/scripts/constants/GodotPaths.cs
@@ -5,4 +5,9 @@ namespace Chess;
 public static class GodotPaths
 {
 	public static readonly StringName OverrideSeparation = new("theme_override_constants/separation");
+	public static readonly StringName OverrideButtonDisabled = new("disabled");
+	public static readonly StringName OverrideButtonFocus = new("focus");
+	public static readonly StringName OverrideButtonHover = new("hover");
+	public static readonly StringName OverrideButtonNormal = new("normal");
+	public static readonly StringName OverrideButtonPressed = new("pressed");
 }
