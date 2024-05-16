090dcf93b8139877ed11bc58f19fdf1fa887da67
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-05-16 16:34:40 -0400

    Update CpuPlayer.cs
    Reflect changes from main branch.

--- game/scripts/cpu/CpuPlayer.cs
+++ game/scripts/cpu/CpuPlayer.cs
@@ -1,11 +1,10 @@
 using Godot;
-using Chess.Nodes;
 
 namespace Chess.Cpu;
 
 public partial class CpuPlayer : Node
 {
-	public Teams Team { get; set; }
+	public Team Team { get; set; }
 	
 	private readonly Brain brain = new();
 }
